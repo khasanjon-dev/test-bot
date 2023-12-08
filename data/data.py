03ac168de5ea17d15d54a40efdbfb2888a638b25
khasanjon <khasanjon.dev@gmail.com>
2023-12-08 14:25:33 +0500

    add some files

--- data/data.py
+++ data/data.py
@@ -1,5 +1,8 @@
 main_menu = {
-    '➕Test yaratish': 'create_test',
-    '✅Javobni tekshirirsh': 'check_answer',
-    '🤖Xizmatlar': 'services',
+    '➕ Test yaratish': 'create_test',
+    '✅ Javobni tekshirirsh': 'check_answer',
+    '🤖 Xizmatlar': 'services',
+    '✅ Sertifikat tanlash': 'choice_certificate',
+    "❗️ Bot haqida ma'lumot": 'about',
+    "🔰 Mening ma'lumotlarim": 'about_me'
 }
