f991d2ec3fb8405316a2537017e609d57b935024
khasanjon <khasanjon.dev@gmail.com>
2023-12-10 10:00:22 +0500

    add some actions

--- utils/callback_class.py
+++ utils/callback_class.py
@@ -3,6 +3,16 @@ from enum import Enum
 from aiogram.filters.callback_data import CallbackData
 
 
+class BackMenu(str, Enum):
+    back = 'back'
+    back_main_menu = 'back_main_menu'
+
+
+class BackMenuCallback(CallbackData, prefix='back'):
+    back: BackMenu
+    back_main_menu: BackMenu
+
+
 class MainMenu(str, Enum):
     create_test = 'create_test'
     check_answer = 'check_answer'
@@ -15,8 +25,9 @@ class MainMenuCallback(CallbackData, prefix='main_menu'):
 
 
 class CreateTest(str, Enum):
-    block_test = 'block_test'
-    science_test = 'science_test'
+    block_test = 'block'
+    science_test = 'science'
+    back = 'back'
 
 
 class CreateTestCallback(CallbackData, prefix='test'):
