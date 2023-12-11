d0e2852db5b70a5162d25d6d0878c9878459e3c7
khasanjon <khasanjon.dev@gmail.com>
2023-12-11 08:32:29 +0500

    add some actions

--- logic/start.py
+++ logic/start.py
@@ -2,6 +2,7 @@ from aiogram import Router, types
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 
+from logic.menu import main_menu_handler
 from utils import states
 
 start = Router()
@@ -10,8 +11,9 @@ start = Router()
 @start.message(CommandStart())
 async def start_handler(msg: types.Message, state: FSMContext):
     await state.set_state(states.MainMenu.main_menu)
-    text = ("Hurmatli foydalanuvchi ism familiyangizni quyidagi ko'rinishda kiriting 👇👇👇\n"
-            "Ism Familiya\n\n"
-            "Misol:\n"
-            "Akbar Avazov")
-    await msg.answer(text)
+    await main_menu_handler(msg)
+    # text = ("Hurmatli foydalanuvchi ism familiyangizni quyidagi ko'rinishda kiriting 👇👇👇\n"
+    #         "Ism Familiya\n\n"
+    #         "Misol:\n"
+    #         "Akbar Avazov")
+    # await msg.answer(text)
